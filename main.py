248ebd14e06021c6df0a50fa0a1a51231d2d3b4b
Evgeniy Grigoriev <grigrme@gmail.com>
2022-06-13 01:24:31 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ BACKGROUND_COLOR = "#000000"
 GRAY_COLOR = "#808080"
 GRAY_COLOR2 = "#C0CCB0"
 RED_COLOR = "#FF0000"
-PYRAMID_COLOR = [("W","#FFFFFF"),("B","#0000FF"),("G","#008000"),("R","#800080")] # "R","#FF0000"
+PYRAMID_COLOR = [("W","#FFFFFF"),("B","#0000FF"),("G","#008000"),("R","#FF0000")] # "R","800080"
 
 PYRAMID_STATE = [["WB","GR"],["WG","RB"],["WR","BG"],
                  ["BW","RG"],["BG","WR"],["BR","GW"],
@@ -109,14 +109,14 @@ def init_level(y,x):
                 else:
                     if (nx//3)%2 == 0:
                         if ny % 2 == 0:
-                            col = "B"
-                        else:
                             col = "G"
+                        else:
+                            col = "B"
                     else:
                         if ny % 2 == 1:
-                            col = "B"
-                        else:
                             col = "G"
+                        else:
+                            col = "B"
                 str.append(["W", col])
         level.append(str)
     ny = y // 2
@@ -551,41 +551,7 @@ def main():
             screen.blit(text_solved, text_solved_place)
 
             ############################################
-            # отрисовка сетки
-            for ny,row in enumerate(level):
-                for nx,pyramid in enumerate(row):
-                    orient = (ny % 2 == 0) == (nx % 2 == 0) # уголок вверх
-                    ############################################
-                    # расчет всех координат
-                    if orient: # уголок вверх
-                        fl_or = 1
-                        if (ny % 2 == 0) and (nx % 2 == 0):  # 1 ряд, наверх
-                            x1 = int(EDGE_PYRAMID / 2) + (nx // 2) * EDGE_PYRAMID + BORDER
-                        elif (ny % 2 == 1) and (nx % 2 == 1):  # 2 ряд, наверх
-                            x1 = (nx // 2 + nx % 2) * EDGE_PYRAMID + BORDER
-                        y1 = ny * HEIGHT_PYRAMID + BORDER
-                        yy = y1 + HEIGHT_PYRAMID
-                        x2  = x1 + int(EDGE_PYRAMID / 2)
-                        x3  = x1 - int(EDGE_PYRAMID / 2)
-                    else:  # уголок вниз
-                        fl_or = -1
-                        if (ny % 2 == 0) and (nx % 2 == 1):  # 1 ряд, вниз
-                            x1 = (nx // 2 + nx % 2) * EDGE_PYRAMID + BORDER
-                        elif (ny % 2 == 1) and (nx % 2 == 0):  # 2 ряд, вниз
-                            x1 = int(EDGE_PYRAMID / 2) + (nx // 2) * EDGE_PYRAMID + BORDER
-                        yy = ny * HEIGHT_PYRAMID + BORDER
-                        y1 = yy + HEIGHT_PYRAMID
-                        x2  = x1 - int(EDGE_PYRAMID / 2)
-                        x3  = x1 + int(EDGE_PYRAMID / 2)
-
-                    # отрисовка
-                    if pyramid[0]!="X":
-                        draw.line(screen, GRAY_COLOR2, [x2, yy], [x3, yy], 2)
-                        draw.line(screen, GRAY_COLOR2, [x1, y1], [x2, yy], 2)
-                        draw.line(screen, GRAY_COLOR2, [x1, y1], [x3, yy], 2)
-
-            ############################################
-            # отрисовка пирамидок
+            # отрисовка сетки и пирамидок
             solved = True
 
             for ny,row in enumerate(level):
@@ -602,6 +568,8 @@ def main():
                             x1 = (nx // 2 + nx % 2) * EDGE_PYRAMID + BORDER
                         y1 = ny * HEIGHT_PYRAMID + BORDER + (2 * BORDER)
                         yy = y1 + HEIGHT_PYRAMIDka
+                        y1_grid = ny * HEIGHT_PYRAMID + BORDER
+                        yy_grid = y1_grid + HEIGHT_PYRAMID
                     else:  # уголок вниз
                         fl_or = -1
                         if (ny % 2 == 0) and (nx % 2 == 1):  # 1 ряд, вниз
@@ -610,6 +578,8 @@ def main():
                             x1 = int(EDGE_PYRAMID / 2) + (nx // 2) * EDGE_PYRAMID + BORDER
                         yy = ny * HEIGHT_PYRAMID + BORDER + (BORDER)
                         y1 = yy + HEIGHT_PYRAMIDka
+                        yy_grid = ny * HEIGHT_PYRAMID + BORDER
+                        y1_grid = yy_grid + HEIGHT_PYRAMID
 
                     x2  = x1 + int(EDGE_PYRAMIDka / 2) * fl_or
                     x3  = x1 - int(EDGE_PYRAMIDka / 2) * fl_or
@@ -626,8 +596,17 @@ def main():
                     xc3 = x1 - int(EDGE_PYRAMIDka / 6) * fl_or
                     yc2 = y1 + int(5 * HEIGHT_PYRAMIDka / 9) * fl_or
 
+                    x2_grid  = x1 + int(EDGE_PYRAMID / 2) * fl_or
+                    x3_grid  = x1 - int(EDGE_PYRAMID / 2) * fl_or
+
                     ############################################
-                    # сама отрисовка
+                    # отрисовка сетки
+                    if pyramid[0]!="X":
+                        draw.line(screen, GRAY_COLOR2, [x2_grid, yy_grid], [x3_grid, yy_grid], 2)
+                        draw.line(screen, GRAY_COLOR2, [x1, y1_grid], [x2_grid, yy_grid], 2)
+                        draw.line(screen, GRAY_COLOR2, [x1, y1_grid], [x3_grid, yy_grid], 2)
+
+                    # отрисовка пирамидок
 
                     if pyramid[0]==" ": # пустая ячейка
                         draw.polygon(screen, BACKGROUND_COLOR, [[x1, y1], [x2, yy], [x3, yy]])
@@ -657,9 +636,10 @@ def main():
                                 elif pyramid[1]==color[0]: # перед
                                     draw.polygon(screen,color[1], [[x1, y1], [x20, y11], [xc2, yc2], [x1, y10], [xc3, yc2], [x30, y11]] )
                                 elif pyramid2[0] == color[0]: # лево
-                                    draw.polygon(screen, color[1], [[x3, yy], [x30, y11], [xc3, yc2], [x30, yy0], [x1, yc], [x1, yy]])
-                                elif pyramid2[1] == color[0]:  # право
                                     draw.polygon(screen,color[1], [[x2, yy], [x1, yy], [x1, yc], [x20, yy0], [xc2, yc2], [x20, y11]] )
+                                elif pyramid2[1] == color[0]:  # право
+                                    draw.polygon(screen, color[1], [[x3, yy], [x30, y11], [xc3, yc2], [x30, yy0], [x1, yc], [x1, yy]])
+
                             draw.aaline(screen, GRAY_COLOR, [x1, y0], [x1, y1])
                             draw.aaline(screen, GRAY_COLOR, [x1, y0], [x2, yy])
                             draw.aaline(screen, GRAY_COLOR, [x1, y0], [x3, yy])
